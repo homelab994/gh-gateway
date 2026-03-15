0931d34dc8d2f6ae29910c0e7ccbc43afe7093a2
grusu94 <georgerusu994@gmail.com>
2026-03-15 15:28:21 +0200

    Initial commit - add service

---
+++ src/main/java/com/github/grusu94/gh/gateway/GhGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.github.grusu94.gh.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GhGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/grusu94/gh/gateway/GhGatewayApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.grusu94.gh.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(properties = {
+        "spring.profiles.active=test",
+        "eureka.client.enabled=false"
+})
+class GhGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
